ebeb29d290e99be189c792fe07bb0fe066757dd3
shadow-yuan <1092421495@qq.com>
2020-05-12 17:48:36 +0800

    status

---
+++ util/status.cc
@@ -0,0 +1,121 @@
+/*
+ *
+ * Copyright (c) 2020 The Raptor Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "util/status.h"
+#include <string.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <utility>
+
+namespace raptor {
+Status::Status()
+    : Status(0, std::string()) {}
+
+Status::Status(const std::string& msg)
+    : Status(-1, msg) {
+}
+
+Status::Status(int err_code, const std::string& err_msg)
+    : _error_code(err_code), _message(err_msg) {
+}
+
+Status::Status(const Status& oth)
+    : _error_code(oth._error_code), _message(oth._message)
+{}
+
+Status& Status::operator= (const Status& oth) {
+    if (this != &oth) {
+        _error_code = oth._error_code;
+        _message = oth._message;
+    }
+    return *this;
+}
+
+Status::Status(Status&& other) {
+    _error_code = std::move(other._error_code);
+    _message = std::move(other._message);
+}
+
+Status& Status::operator=(Status&& other) {
+    _error_code = std::move(other._error_code);
+    _message = std::move(other._message);
+    return *this;
+}
+
+bool Status::IsOK() const {
+    return _error_code == 0;
+}
+
+int Status::ErrorCode() const {
+    return _error_code;
+}
+
+std::string Status::ToString() const {
+    if (_error_code == 0) {
+        return std::string("No error");
+    }
+
+    char* text = nullptr;
+    raptor_asprintf(&text, "%s: error code is %d.", _message.c_str(), _error_code);
+    std::string rsp(text);
+    raptor::Free(text);
+    return rsp;
+}
+
+void Status::AppendMessage(const std::string& msg) {
+    _message.append(msg);
+}
+
+bool Status::operator==(const Status& other) const {
+    return _error_code == other.ErrorCode();
+}
+
+bool Status::operator!=(const Status& other) const {
+    return _error_code != other.ErrorCode();
+}
+
+RefCountedPtr<Status> MakeStatusFromStaticString(const char* msg) {
+    return MakeRefCounted<Status>(msg);
+}
+
+RefCountedPtr<Status> MakeStatusFromOSError(const char* api) {
+    int err = errno;
+    char* text = nullptr;
+    raptor_asprintf(&text, "%s: %s", api, strerror(err));
+    RefCountedPtr<Status> obj = MakeRefCounted<Status>(err, text);
+    raptor::Free(text);
+    return obj;
+}
+
+#ifdef _WIN32
+RefCountedPtr<Status> MakeStatusFromWSAError(int err, const char* api) {
+    int err = WSAGetLastError();
+    char* message = raptor_format_message(err);
+
+    char* text = nullptr;
+    raptor_asprintf(&text, "%s: %s", api, message);
+
+    RefCountedPtr<Status> obj = MakeRefCounted<Status>(err, text);
+    raptor::Free(message);
+    raptor::Free(text);
+    return obj;
+}
+#endif
+
+}  // namespace raptor
---
+++ util/status.h
@@ -0,0 +1,100 @@
+/*
+ *
+ * Copyright (c) 2020 The Raptor Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef __RAPTOR_UTIL_STATUS__
+#define __RAPTOR_UTIL_STATUS__
+
+#include <string>
+
+#include "util/alloc.h"
+#include "util/string.h"
+#include "util/ref_counted.h"
+
+namespace raptor {
+class Status final : public RefCounted<Status, NonPolymorphicRefCount> {
+public:
+    Status();
+    Status(const std::string& msg);
+    Status(int err_code, const std::string& err_msg);
+
+    Status(const Status& oth);
+    Status& operator= (const Status& oth);
+    Status(Status&& other);
+    Status& operator=(Status&& other);
+    ~Status() = default;
+
+    bool IsOK() const;
+    int ErrorCode() const;
+    std::string ToString() const;
+    void AppendMessage(const std::string& msg);
+
+    bool operator==(const Status& other) const;
+    bool operator!=(const Status& other) const;
+
+ private:
+    // 0: no error
+    int _error_code;
+    std::string _message;
+
+    friend RefCountedPtr<Status> MakeStatusFromStaticString(const char* msg);
+    friend RefCountedPtr<Status> MakeStatusFromOSError(const char* api);
+
+#ifdef _WIN32
+    friend RefCountedPtr<Status> MakeStatusFromWSAError(int err, const char* api);
+#endif
+};
+
+RefCountedPtr<Status> MakeStatusFromStaticString(const char* msg);
+RefCountedPtr<Status> MakeStatusFromOSError(const char* api);
+
+#ifdef _WIN32
+RefCountedPtr<Status> MakeStatusFromWSAError(int err, const char* api);
+#endif
+
+template <typename... Args>
+inline RefCountedPtr<Status> MakeStatusFromFormat(Args&&... args) {
+    char* message = nullptr;
+    raptor_asprintf(&message, std::forward<Args>(args)...);
+    if (!message) {
+        return nullptr;
+    }
+    RefCountedPtr<Status> obj = MakeRefCounted<Status>(message);
+    raptor::Free(message);
+    return obj;
+}
+
+}  // namespace raptor
+
+#define RAPTOR_OS_ERROR(api_name) \
+    raptor::MakeStatusFromOSError(api_name)
+
+#define RAPTOR_ERROR_FROM_STATIC_STRING(message) \
+    raptor::MakeStatusFromStaticString(message)
+
+#define RAPTOR_ERROR_FROM_FORMAT(FMT, ...) \
+    raptor::MakeStatusFromFormat(FMT, ##__VA_ARGS__)
+
+#ifdef _WIN32
+#define RAPTOR_WSA_ERROR(err, api_name) \
+    raptor::MakeStatusFromWSAError(err, api_name)
+#endif
+
+using raptor_error = raptor::RefCountedPtr<raptor::Status>;
+#define RAPTOR_ERROR_NONE nullptr
+
+#endif  // __RAPTOR_UTIL_STATUS__
