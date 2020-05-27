d2683d7372ecdd4bc6fcd086c2ecab4b8cb6aed2
shadow-yuan <1092421495@qq.com>
2020-05-27 19:32:41 +0800

    move files

--- plugin/protocol.h
+++ plugin/protocol.h
@@ -2,25 +2,21 @@
 #define __RAPTOR_PLUGIN_PROTOCOL__
 
 #include <stddef.h>
+#include "raptor/slice.h"
 
 namespace raptor {
-class Slice;
 class Protocol {
 public:
     virtual ~Protocol() {}
 
-    // Only return the header size of the current layer protocol
-    virtual size_t GetHeaderSize();
+    // Get the max header size of current protocol
+    virtual size_t GetMaxHeaderSize() = 0;
 
     // Before sending data, you need to build a header
     virtual Slice BuildPackageHeader(size_t pack_len) = 0;
 
     // return -1: error;  0: need more data; > 0 : pack_len
     virtual int CheckPackageLength(const Slice* obj) = 0;
-
-    // protocol parsing chain
-    virtual bool OnDataParsing(const Slice* input, Slice* output) = 0;
-
 };
 } // namespace raptor
 #endif  // __RAPTOR_PLUGIN_PROTOCOL__
